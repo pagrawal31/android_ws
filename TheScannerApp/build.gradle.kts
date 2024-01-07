90aab5832ded0bbfdbc3d9eee585ab2e28b6c36a
peeyush-agrawal <peeyushagrawal@sharechat.co>
2024-01-07 18:54:22 +0530

    Baseline for scanner app

---
+++ TheScannerApp/build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+
+    // implementation 'com.google.zxing:core:3.4.1'
+    // implementation 'com.journeyapps:zxing-android-embedded:4.0.0'
+
+
+}
